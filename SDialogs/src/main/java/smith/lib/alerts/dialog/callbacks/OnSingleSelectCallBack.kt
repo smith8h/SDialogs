dcd5ad54f8493680bd8ccf1a1c9ad51d31c4a907
Dev. Smith <husseinshakir81@gmail.com>
2023-07-29 16:25:34 +0300

    - Interfaces Javadocs

--- SDialogs/src/main/java/smith/lib/alerts/dialog/callbacks/OnSingleSelectCallBack.kt
+++ SDialogs/src/main/java/smith/lib/alerts/dialog/callbacks/OnSingleSelectCallBack.kt
@@ -1,5 +1,13 @@
 package smith.lib.alerts.dialog.callbacks
 
+/**
+ * Single item selection callback to get the value selected.
+ */
 interface OnSingleSelectCallBack {
+    /**
+     * triggered when selecting an item from the selection.
+     * @param itemId the id of that selected item.
+     * @param itemText the text of that selected item.
+     */
     fun onSelect(itemId: Int, itemText: String)
 }
--- SDialogs/src/main/java/smith/lib/alerts/dialog/callbacks/OnSlideCallBack.kt
+++ SDialogs/src/main/java/smith/lib/alerts/dialog/callbacks/OnSlideCallBack.kt
@@ -1,5 +1,12 @@
 package smith.lib.alerts.dialog.callbacks
 
+/**
+ * Slider callback to get the value selected.
+ */
 interface OnSlideCallBack {
+    /**
+     * triggered when clicking on ok button to get the value selected from the slider.
+     * @param value The value returned from slider as Float.
+     */
     fun onValueSelected(value: Float)
 }
